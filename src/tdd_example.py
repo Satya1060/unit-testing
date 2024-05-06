fa549024ad85b5281659c77f90e41592800f4b00
Satya <satyanarayana@datagrokr.com>
2024-05-06 15:36:13 -0700

    Added functionality to test_general_example.py

--- src/tdd_example.py
+++ src/tdd_example.py
@@ -6,23 +6,24 @@ class TDDExample():
         """
         Reverses order of characters in string input_str.
         """
-        return None
+        return input_str[::-1]
 
     def find_longest_word(self, sentence: str) -> str:
         """
         Returns the longest word in string sentence.
         In case there are several, return the first.
         """
-        return None
+        words = sentence.split()
+        return max(words, key=len)
 
     def reverse_list(self, input_list: list) -> list:
         """
         Reverses order of elements in list input_list.
         """
-        return []
+        return input_list[::-1]
 
     def count_digits(self, input_list: list, number_to_be_counted: int) -> int:
         """
         Return count of digits
         """
-        return 0
+        return input_list.count(number_to_be_counted)
